fab7661bc46f97dff7dfbac873c8dc0353fd44ce
GbeTech <gbetech@gmail.com>
2018-05-18 23:29:28 +0200

    good

--- autosyntax/homepage/static/homepage/js/layout.js
+++ autosyntax/homepage/static/homepage/js/layout.js
@@ -4,32 +4,60 @@ on_resize();
 function on_resize() {
     let w = window.screen.availWidth;
     console.log(w);
-    if (w < 620) {
-        set_style(document.body, "marginLeft", Math.pow(w, 0.6));
+    resize_body();
+    resize_pad_body_space();
+    resize_works();
+    resize_console_menu();
+
+    function resize_console_menu() {
+        if (w < 700 && w > 550) {
+            set_style("console_menu", "height", w / 24);
+        }
+        else if (w <= 550) {
+            set_style("console_menu", "height", w / 20);
+        }
+        else {
+            set_style("console_menu", "height", 30);
+
+        }
     }
-    else {
-        set_style(document.body, "marginLeft", 60);
+
+    function resize_body() {
+        if (w < 620 && w > 550) {
+            set_style(document.body, "marginLeft", Math.pow(w, 0.6));
+        }
+        else if (w <= 550) {
+            set_style(document.body, "marginLeft", Math.pow(w, 0.5));
+        }
+        else {
+            set_style(document.body, "marginLeft", 60);
+        }
     }
-    let div_by_avg = w / 11;
-    if (w < 1300) {
-        set_style("pad_body", "paddingLeft", div_by_avg);
-        set_style_to_collection("space", "fontSize", w / 20);
+
+    function resize_pad_body_space() {
+        if (w < 1300) {
+            set_style("pad_body", "paddingLeft", w / 8);
+            set_style_to_collection("space", "fontSize", w / 20);
+        }
+        else {
+            set_style("pad_body", "paddingLeft", 250);
+        }
     }
-    else {
-        set_style("pad_body", "paddingLeft", 250);
-    }
-    let works = by_class("work");
-    if (w < 1300 && w > 800) {
-        set_style_to_collection(works, "fontSize", Math.pow(w, 0.47));
-    }
-    else if (w <= 800 && w > 650) {
-        set_style_to_collection(works, "fontSize", Math.pow(w, 0.46));
-    }
-    else if (w <= 650) {
-        set_style_to_collection(works, "fontSize", Math.pow(w, 0.43));
-    }
-    else {
-        set_style_to_collection(works, "fontSize", 30);
+
+    function resize_works() {
+        let works = by_class("work");
+        if (w < 1300 && w > 800) {
+            set_style_to_collection(works, "fontSize", Math.pow(w, 0.47));
+        }
+        else if (w <= 800 && w > 650) {
+            set_style_to_collection(works, "fontSize", Math.pow(w, 0.46));
+        }
+        else if (w <= 650) {
+            set_style_to_collection(works, "fontSize", Math.pow(w, 0.43));
+        }
+        else {
+            set_style_to_collection(works, "fontSize", 30);
+        }
     }
 
 }
