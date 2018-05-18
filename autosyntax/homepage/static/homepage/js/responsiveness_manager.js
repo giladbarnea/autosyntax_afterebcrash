33c236e1667cdaf131b8aa31b17e6570fc0ab493
GbeTech <gbetech@gmail.com>
2018-05-19 00:53:13 +0200

    also continue arrow

--- autosyntax/homepage/static/homepage/js/responsiveness_manager.js
+++ autosyntax/homepage/static/homepage/js/responsiveness_manager.js
@@ -4,8 +4,6 @@ on_resize();
 function on_resize() {
     let w = window.screen.availWidth;
     console.log(w);
-    by_id("width_flag").innerHTML = w;
-    set_style("width_flag", "fontSize", w / 21.7);
     resize_body_left();
     resize_body_right();
     resize_pad_body();
@@ -17,12 +15,16 @@ function on_resize() {
     function resize_console_menu() {
         if (w < 700 && w > 550) {
             set_style("console_menu", "height", w / 24);
+            set_style("continue_arrow", "height", w / 14.4);
+
         }
         else if (w <= 550) {
             set_style("console_menu", "height", w / 20);
+            set_style("continue_arrow", "height", w / 12);
         }
         else {
             set_style("console_menu", "height", 30);
+            set_style("continue_arrow", "height", 50);
         }
     }
 
@@ -121,50 +123,3 @@ function on_resize() {
     }
 
 }
-
-set_style("whatisit_block", "paddingTop", window.screen.availHeight);
-increase_opacity(by_id("pad_body"), 1);
-
-on_call("continue_arrow", "click", scroll_to_whatisit);
-
-let is_cons_menu_vis = false;
-on_call("console_menu", "mouseover", show_console_menu);
-on_call("console_menu", "click", hide_console_menu);
-on_call("sidebar_item_1", "click", scroll_to_whatisit);
-
-function decrease_space_font_10() {
-    let spaces = by_class("space");
-    // console.log(typeof spaces[0].style.fontSize * 0.5);
-    set_style_to_collection("space", "fontSize",
-        parseFloat(spaces[0].style.fontSize) * 0.95);
-}
-
-on_call("sidebar_item_2", "click", decrease_space_font_10);
-
-
-function hide_console_menu() {
-    if (is_cons_menu_vis) { //if menu visible
-        // hide menu
-        let sidebar_items = by_class("sidebar-item");
-        for (let i = 0; i < sidebar_items.length; i++) {
-            decrease_opacity(sidebar_items[i], 0);
-        }
-        // flag menu hidden
-        is_cons_menu_vis = false;
-    }
-}
-
-function show_console_menu() {
-    if (!is_cons_menu_vis) { //if menu hidden
-        // show menu
-        let sidebar_items = by_class("sidebar-item");
-        for (let i = 0; i < sidebar_items.length; i++) {
-            increase_opacity(sidebar_items[i], 1);
-        }
-        // flag menu visible
-        is_cons_menu_vis = true;
-    }
-
-
-}
-
