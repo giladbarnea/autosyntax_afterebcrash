9aa2cf5378c393be8dc882e0266888174a797c20
GbeTech <gbetech@gmail.com>
2018-05-20 06:59:19 +0200

    ok

--- autosyntax/autosyntax/__init__.py
+++ autosyntax/autosyntax/__init__.py
@@ -11,3 +11,12 @@
 # numbers="hi"
 # for number in range(len(numbers)):
 #
+# def is_easy(autosyntax, *args, **kwargs):
+# 	"""
+#
+# 	"""
+# 	hi="hi"
+# letters = ""
+# printed = [print(letter) for letter in letters]
+w='s'
+hi = f'{w} you'
