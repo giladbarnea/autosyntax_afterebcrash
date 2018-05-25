302172cfe2248c6e491d6c996b21ea905e21d275
GbeTech <gbetech@gmail.com>
2018-05-25 14:59:03 +0300

    good toggle

--- autosyntax/homepage/static/homepage/js/howtouse_fns.js
+++ autosyntax/homepage/static/homepage/js/howtouse_fns.js
@@ -5,49 +5,12 @@
 window.onload = () => {
     by_id("continue_arrow").style["display"] = "none";
 
-    // 1:2
     toggle_pair("animation_1", "animation_2");
-    // on_event_do("animation_1", "click",
-    //     () => _animations_toggle("animation_1",
-    //         "animation_2"));
-    // on_event_do("animation_2", "click",
-    //     () => _animations_toggle("animation_2",
-    //         "animation_1"));
-
-    // 3:4
     toggle_pair("animation_3", "animation_4");
-    // on_event_do("animation_3", "click",
-    //     () => _animations_toggle("animation_3",
-    //         "animation_4"));
-    // on_event_do("animation_4", "click",
-    //     () => _animations_toggle("animation_4",
-    //         "animation_3"));
-    // 5:6
     toggle_pair("animation_5", "animation_6");
-    // on_event_do("animation_5", "click",
-    //     () => _animations_toggle("animation_5",
-    //         "animation_6"));
-    // on_event_do("animation_6", "click",
-    //     () => _animations_toggle("animation_6",
-    //         "animation_5"));
-
-    // 7:8
     toggle_pair("animation_7", "animation_8");
-    // on_event_do("animation_7", "click",
-    //     () => _animations_toggle("animation_7",
-    //         "animation_8"));
-    // on_event_do("animation_8", "click",
-    //     () => _animations_toggle("animation_8",
-    //         "animation_7"));
-
-    // 9:10
     toggle_pair("animation_9", "animation_10");
-    // on_event_do("animation_7", "click",
-    //     () => _animations_toggle("animation_7",
-    //         "animation_8"));
-    // on_event_do("animation_8", "click",
-    //     () => _animations_toggle("animation_8",
-    //         "animation_7"));
+
     set_style("back", "display", "unset");
 
     let sidebar_items = by_id("sidebar").children;
@@ -63,8 +26,8 @@ window.onload = () => {
 };
 
 function toggle_pair(a, b) {
-    a = element_or_by_fn(a);
-    b = element_or_by_fn(b);
+    a = element_or_by_fn(a, by_id);
+    b = element_or_by_fn(b, by_id);
     on_event_do(a, "click",
         () => _animations_toggle(a,
             b));
