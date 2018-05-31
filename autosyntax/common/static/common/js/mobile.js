65791395aee90ac31a09357cd54a4dd07cd9a841
GbeTech <gbetech@gmail.com>
2018-05-31 17:33:23 +0300

    finished cleanup

--- autosyntax/common/static/common/js/mobile.js
+++ autosyntax/common/static/common/js/mobile.js
@@ -21,9 +21,10 @@ function tablet_portrait() {
 
     // arrow.set(tablet_landscape_up);
     // BODY
-    document.body.style['marginLeft'] = "30px";
-    document.body.style['marginRight'] = "10px";
-    document.body.style['marginTop'] = "10px";
+    body_margins(top = 10, right = 10, bottom = 60, left = 30);
+    // document.body.style['marginLeft'] = "30px";
+    // document.body.style['marginRight'] = "10px";
+    // document.body.style['marginTop'] = "10px";
 
 
     let filename = window.location.pathname
@@ -32,88 +33,97 @@ function tablet_portrait() {
         .pop();
 
     // FONT SIZE
-    set_style_to_collection('fs60', 'fontSize', 45);
-
-    // lorem
-    set_style_to_collection('fs35', 'fontSize', 35);
-    // BR 2.5
-    set_style_to_collection('fs32', 'fontSize', 28);
-
-    set_style_to_collection('fs30', 'fontSize', 22);
-    set_style_to_collection('fs25', 'fontSize', 20);
-    set_style_to_collection('fs23', 'fontSize', 18);
-
-    set_style_to_collection('fs20', 'fontSize', 14);
-    set_style_to_collection('fs15', 'fontSize', 15);
+    fonts_sizes(_60 = 45, _35 = 35, _32 = 28, _30 = 22, _25 = 20, _23 = 18, _20 = 14, _15 = 15);
+    // set_style_to_collection('fs60', 'fontSize', 45);
+    //
+    // // lorem
+    // set_style_to_collection('fs35', 'fontSize', 35);
+    // // BR 2.5
+    // set_style_to_collection('fs32', 'fontSize', 28);
+    //
+    // set_style_to_collection('fs30', 'fontSize', 22);
+    // set_style_to_collection('fs25', 'fontSize', 20);
+    // set_style_to_collection('fs23', 'fontSize', 18);
+    //
+    // set_style_to_collection('fs20', 'fontSize', 14);
+    // set_style_to_collection('fs15', 'fontSize', 15);
     //
 
 
     // PAD-BODY
-    set_style_to_collection('pad-body', 'paddingLeft', 0);
+    pad_body(0);
+    // set_style_to_collection('pad-body', 'paddingLeft', 0);
 
     add_class('pad_body', 'row-1');
 
 
     //
-    set_style_to_collection('top88', 'top', "90%");
-    set_style_to_collection('top84', 'top', "81%");
+    tops(_88 = '90%', _84 = '81%');
+    // set_style_to_collection('top88', 'top', "90%");
+    // set_style_to_collection('top84', 'top', "81%");
 
+    _left('85%');
 
-    set_style_to_collection('l90', 'left', "85%");
+    // set_style_to_collection('l90', 'left', "85%");
     //
 
     // HEIGHT WIDTH
-    set_style_to_collection('height60', 'height', 40);
+    heights(_60 = 40, _30 = 25);
+    // set_style_to_collection('height60', 'height', 40);
     // hamburger is 50
-    set_style_to_collection('height50', 'height', 45);
-    set_style_to_collection('height30', 'height', 25);
-    //
-    set_style_to_collection('width60', 'width', 40);
+    // set_style_to_collection('height30', 'height', 25);
+    widths(_60 = 40, _30 = 25);
+    // set_style_to_collection('width60', 'width', 40);
     // hamburger is 50
-    set_style_to_collection('width50', 'width', 45);
-    set_style_to_collection('width30', 'width', 25);
+    // set_style_to_collection('width30', 'width', 25);
     //
 
     // PT PADDING TOP
+    padding_tops(_20 = 15);
     set_style_to_collection('pt20', 'paddingTop', 15);
 
 
     // PL PADDING LEFT
-    set_style_to_collection('pl20', 'paddingLeft', 15);
-    set_style_to_collection('pl30', 'paddingLeft', 23);
-
     let padding_left_40 = "25px";
-    set_style_to_collection('pl40', 'paddingLeft', padding_left_40);
-    set_style_to_collection('pl65', 'paddingLeft', 65);
+    padding_lefts(_20 = 15, _30 = 23, _40 = padding_left_40, _45 = 35, _65 = 65);
+    // set_style_to_collection('pl20', 'paddingLeft', 15);
+    // set_style_to_collection('pl30', 'paddingLeft', 23);
+
+    // set_style_to_collection('pl40', 'paddingLeft', padding_left_40);
+    // set_style_to_collection('pl65', 'paddingLeft', 65);
 
 
     //
     // //NO USE TO PL45
-    set_style_to_collection('pl45', 'paddingLeft', 35);
+    // set_style_to_collection('pl45', 'paddingLeft', 35);
     //
 
     // MR MARGIN RIGHT
     // because pad-body is 180
-    set_style_to_collection('mr400', 'marginRight', 30);
-    set_style_to_collection('mr300', 'marginRight', 30);
-    set_style_to_collection('mr200', 'marginRight', 60);
-    set_style_to_collection('mr500', 'marginRight', 60);
+    margin_rights(_200 = 60, _300 = 30, _400 = 30, _500 = 60);
+    // set_style_to_collection('mr400', 'marginRight', 30);
+    // set_style_to_collection('mr300', 'marginRight', 30);
+    // set_style_to_collection('mr200', 'marginRight', 60);
+    // set_style_to_collection('mr500', 'marginRight', 60);
 
 
     // MT MARGIN TOP
-    set_style_to_collection('mt20', 'marginTop', 15);
-    set_style_to_collection('mt25', 'marginTop', 20);
+    margin_tops(_20 = 15, _25 = 20);
+    // set_style_to_collection('mt20', 'marginTop', 15);
+    // set_style_to_collection('mt25', 'marginTop', 20);
 
 
     // MY-UL
-    set_style_to_collection('my-ul', 'paddingLeft', "1em");
+    my_ul('1em');
+    // set_style_to_collection('my-ul', 'paddingLeft', "1em");
 
     // ML MARGIN LEFT
-    set_style_to_collection('ml100', 'marginLeft', 70);
+    margin_lefts(_100 = 70, _136 = 110, _150 = 120);
+    // set_style_to_collection('ml100', 'marginLeft', 70);
 
     // //PRESS TO TOGGLE .NUTELLA COMPENSATION
-    set_style_to_collection('ml150', 'marginLeft', 120);
-    set_style_to_collection('ml136', 'marginLeft', 110);
+    // set_style_to_collection('ml150', 'marginLeft', 120);
+    // set_style_to_collection('ml136', 'marginLeft', 110);
 
 
     let works = by_class('work');
